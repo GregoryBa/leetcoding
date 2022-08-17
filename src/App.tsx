ac96c6035cfbf38b84d80ded6fe2e1683e3fa301
GregoryBa <gregory.ba.95@gmail.com>
2022-08-17 14:38:33 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+    </div>
+  );
+}
+
+export default App;
