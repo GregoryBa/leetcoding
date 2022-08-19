110a2f24a5add5609f5805df1c861580841b93f4
GregoryBa <gregory.ba.95@gmail.com>
2022-08-19 09:11:00 +0200

    added form

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,17 @@ function App() {
           React app for testing of my daily leetcoding
         </h1>
         <p>Stack: React, Typescript, TailwindCSS</p>
+        <form>
+          <label>
+          Number 1:
+          <input type="text" name="nr1" />
+          </label>
+          <label>
+          Number 2:
+          <input type="text" name="nr2" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
     </div>
   );
 }
